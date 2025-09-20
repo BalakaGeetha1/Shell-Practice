26f8e2ebb210a15c3f082533403f42bb0a5a4e4a
geetha <geetharanib70@gmail.com>
2025-09-20 23:19:33 +0530

    3rd script

--- 08-Special-Variables.sh
+++ 08-Special-Variables.sh
@@ -3,7 +3,7 @@
 echo " All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 
-echo "Current diraectory: $pwd "
+echo "Current diraectory: $PWD"
 echo "who is running this: $USER"
 echo "Home directory of user: $HOME"
 echo "Pid of the script is: $$"
