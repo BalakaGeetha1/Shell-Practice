f594958f1113a67b0a06b841521369599d3d33c4
geetha <geetharanib70@gmail.com>
2025-09-20 22:45:10 +0530

    3rd script

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=John
+Person2=Raheem
+
+echo "$Person1:: Hi $Person2, how are you?"
+echo "$Person2:: Hello $Person1, Ia m fine. How are you?"
+echo "$Person1:: What are you learning recently?"
+echo "$Person2:: I am learning shell scripting"
