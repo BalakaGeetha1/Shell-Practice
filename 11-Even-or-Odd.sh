7ca8148d04217057ee4dd843bef29d776c82dacd
geetha <geetharanib70@gmail.com>
2025-09-22 12:07:05 +0530

    conditions script

--- 11-Even-or-Odd.sh
+++ 11-Even-or-Odd.sh
@@ -3,7 +3,7 @@
 echo " enter a number:"
 read Number
 
-if [$Number % 2 -eq 0]; then
+if [ $Number % 2 -eq 0]; then
     echo "Given Number $Number is even"
 else
     echo "Given Number $Number is odd"
