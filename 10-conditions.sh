9a5136359ad164f0b6078bdf5d0e5c3628f905bd
geetha <geetharanib70@gmail.com>
2025-09-22 11:05:26 +0530

    conditions script

---
+++ 10-conditions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Number=$1
+if[ $Number -lt 10 ];then
+    echo " Given number $Number is lessthan 10"
+else
+    echo "Given number $Number is greater than or equal to 10"
+fi
---
+++ 11-Even-or-Odd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo " enter a number:"
+read Number
+
+if [ $Number%2 -eq 0 ];then
+    echo "Given Number $Number is even"
+else
+    echo "Given Number $Number is odd"
+fi
