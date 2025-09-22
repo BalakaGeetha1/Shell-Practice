ffd411862674e2cfc0c1147b16bc7c6f28516caa
geetha <geetharanib70@gmail.com>
2025-09-22 23:15:04 +0530

    installing softwares

---
+++ 12-installation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "Error:: please run the script with root priveleges"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing mysql is a failure"
+    exit 1
+else
+    echo "Installing mysql is a failure"
+fi
