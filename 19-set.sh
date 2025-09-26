d35eee9dfa0d5be8f366308594c7a9300dd8e766
geetha <geetharanib70@gmail.com>
2025-09-26 15:11:12 +0530

    19th - set script

--- 19-set.sh
+++ 19-set.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-error(){
-    echo "there is an error"
-}
 
-trap err ERR
+
+# error(){
+#     echo "there is an error"
+# }
+
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"'
 echo "Hello.."
 echo "Before Error"
-shsjdnk;#here shell understands there is qn error
+shsjdnk;gh #here shell understands there is qn error
 echo "after error"
